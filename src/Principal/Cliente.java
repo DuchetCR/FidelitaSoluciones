f05c9d07ee169d611976dd7703c0c16daa4beaf2
DuchetCR <kennethrubi77@gmail.com>
2024-07-05 10:42:05 -0600

    Add calculations for total products, average stay time, and average checkout time

--- src/Principal/Cliente.java
+++ src/Principal/Cliente.java
@@ -9,6 +9,7 @@ public class Cliente extends Thread {
     private int productosComprados;
     private Supermercado supermercado;
     private int pasillosVisitados;
+    private long tiempoTotalEstancia;
 
     public Cliente(String nombre, int productosComprados, int pasillosVisitados, Supermercado supermercado) {
         this.nombre = nombre;
@@ -23,14 +24,25 @@ public class Cliente extends Thread {
             long tiempoLlegada = (long) (Math.random() * 301) + 300;
             Thread.sleep(tiempoLlegada);
 
+            long tiempoEntrada = System.currentTimeMillis();
+
             for (int i = 0; i < pasillosVisitados; i++) {
-                long tiempoPasillo = (long) (Math.random() * 201) + 100; 
+                long tiempoPasillo = (long) (Math.random() * 201) + 100;
                 Thread.sleep(tiempoPasillo);
             }
 
             supermercado.pasarPorCajas(nombre, productosComprados);
+
+            long tiempoSalida = System.currentTimeMillis();
+            tiempoTotalEstancia = tiempoSalida - tiempoEntrada;
+            supermercado.registrarEstancia(tiempoTotalEstancia);
         } catch (InterruptedException e) {
             System.out.println("El cliente " + nombre + " fue interrumpido.");
         }
     }
+
+    public long getTiempoTotalEstancia() {
+        return tiempoTotalEstancia;
+    }
 }
+
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,5 +1,7 @@
 package Principal;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author DuchetCR
@@ -7,13 +9,31 @@ package Principal;
 public class Principal {
     public static void main(String[] args) {
         Supermercado supermercado = new Supermercado();
+        Thread[] clientes = new Thread[100];
 
         for (int i = 0; i < 100; i++) {
             String nombreCliente = "Cliente " + (i + 1);
             int productosComprados = (int) (Math.random() * 7);
             int pasillosVisitados = (int) (Math.random() * 6) + 8;
             Cliente cliente = new Cliente(nombreCliente, productosComprados, pasillosVisitados, supermercado);
+            clientes[i] = cliente;
             cliente.start();
         }
+
+        for (Thread cliente : clientes) {
+            try {
+                cliente.join();
+            } catch (InterruptedException e) {
+                System.out.println("Error esperando a que los clientes terminen.");
+            }
+        }
+
+        int totalProductos = supermercado.getProductosTotales();
+        double tiempoPromedioEstancia = supermercado.getTiempoPromedioEstancia();
+        double tiempoPromedioAtencion = supermercado.getTiempoPromedioAtencion();
+
+        System.out.println("El total de productos adquiridos por todos los clientes es: " + totalProductos);
+        System.out.println("El tiempo promedio de estancia de los clientes en el supermercado es: " + tiempoPromedioEstancia + " ms");
+        System.out.println("El tiempo promedio de atención de la cajera es: " + tiempoPromedioAtencion + " ms");
     }
 }
